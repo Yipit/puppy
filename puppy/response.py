c83f2c19c607533c1a0ce4294c7bec53cd35d881
Sam Narisi <snarisi@gmail.com>
2019-04-16 15:06:32 -0400

    Implement additional page methods, clean up api, and add docstrings

---
+++ puppy/response.py
@@ -0,0 +1,29 @@
+class Response:
+    def __init__(self, response_data, request, page):
+        self._response_data = response_data
+        self._page = page
+        self.request = request
+
+    @property
+    def url(self):
+        return self._response_data['url']
+
+    @property
+    def status(self):
+        return self._response_data['status']
+
+    @property
+    def status_text(self):
+        return self._response_data['statusText']
+
+    @property
+    def headers(self):
+        return self._response_data['headers']
+
+    @property
+    def mime_type(self):
+        return self._response_data['mimeType']
+
+    def text(self):
+        response = self._page.session.send('Network.getResponseBody', requestId=self.request.request_id)
+        return response.get('body')
